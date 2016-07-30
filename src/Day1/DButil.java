8ba5e55200a43056b43efa93d49a5d3e2b0ad7fa
duanSaiSai <2692613726@qq.com>
2016-07-30 09:36:41 +0800

    2

--- src/Day1/DButil.java
+++ src/Day1/DButil.java
@@ -9,15 +9,16 @@ import java.util.Properties;
 
 /** 
  * @author Michael2397 2692613726@qq.com: 
- * @version 创建时间：2016年7月16日 下午9:22:11 
- * 类说明 
+ * @version 鍒涘缓鏃堕棿锛�016骞�鏈�6鏃�涓嬪崍9:22:11 
+ * 绫昏鏄�
  */
 public class DButil {
 	static Properties properties;
 	static{
+		System.out.println("URL");
 		properties = new Properties();
 		Class clazz = DButil.class;
-		InputStream inputStream = clazz.getResourceAsStream("/db.properties");//该方法中的路径就是使用了类文件路径  '/'代表了classpath的路径
+		InputStream inputStream = clazz.getResourceAsStream("/db.properties");//璇ユ柟娉曚腑鐨勮矾寰勫氨鏄娇鐢ㄤ簡绫绘枃浠惰矾寰� '/'浠ｈ〃浜哻lasspath鐨勮矾寰�
 		try {
 			properties.load(inputStream);
 		} catch (IOException e) {
@@ -28,6 +29,6 @@ public class DButil {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("用户名："+properties.getProperty("userName")+"密码"+properties.getProperty("password"));
+		System.out.println("鐢ㄦ埛鍚嶏細"+properties.getProperty("userName")+"瀵嗙爜"+properties.getProperty("password"));
 	}
 }
